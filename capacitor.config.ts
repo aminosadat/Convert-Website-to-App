013052f3a2eb0eed540b1ddc0e7edb2a02a14322
aminosadat <97292044+aminosadat@users.noreply.github.com>
2022-01-07 22:35:11 +1000

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.jamesonsaunders.convertwebsitetoapp',
-  appName: 'Website to App',
+  appId: 'rise.articulate.com/share/u39cVvJ1i3O8Kv4ZcQzr1VMa6owS30bt',
+  appName: 'ENGG1100',
   webDir: 'www',
   bundledWebRuntime: false,
 };
